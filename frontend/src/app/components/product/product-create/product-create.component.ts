af8ba6f72870a4db5bd3fe7b070f68b5ecc85286
Renata Nésio <renata.nesio2@gmail.com>
2023-08-18 09:30:35 -0300

    Update product create component

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -18,22 +18,19 @@ export class ProductCreateComponent implements OnInit {
   constructor(
     private productService: ProductService,
     private router: Router) { }
-    
-    ngOnInit(): void {
-    }
 
-    createProduct(): void {
-      this.productService.create(this.product).subscribe(() => {
-        this.productService.showMessage('Produto criado!');
-        this.router.navigate(["/products"]);
-      });
-    }
- 
+  ngOnInit(): void {
+  }
+
+  createProduct(): void {
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Produto criado!');
+      this.router.navigate(["/products"]);
+    });
+  }
 
   cancel(): void {
     this.router.navigate(["/products"]);
-
   }
-
 
 }
