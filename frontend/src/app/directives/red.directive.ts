a7b37047144c1f0fddf8f8d932deae185d01581b
Renata Nésio <renata.nesio2@gmail.com>
2023-08-18 08:06:11 -0300

    Add red directive

---
+++ frontend/src/app/directives/red.directive.ts
@@ -0,0 +1,14 @@
+import { Directive } from '@angular/core';
+import { ElementRef } from '@angular/core';
+
+
+@Directive({
+  selector: '[appRed]'
+})
+export class RedDirective {
+
+  constructor(private el: ElementRef) { 
+    el.nativeElement.style.color = "#e35e6b";
+  }
+
+}
