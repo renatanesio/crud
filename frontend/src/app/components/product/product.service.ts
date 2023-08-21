62f84e009f17bda7ee05756591f8f97b2a18957e
Renata Nésio <renata.nesio2@gmail.com>
2023-08-21 07:47:14 -0300

    Add product observable to product service

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -23,7 +23,10 @@ export class ProductService {
   }
 
   create(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.baseUrl, product)
+    return this.http.post<Product>(this.baseUrl, product);
   }
 
+  read(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl);
+  }
 }
